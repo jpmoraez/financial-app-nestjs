1ef23d2dc0bf646e444757c0adbcb352ace6eba0
Joao Paulo <jpmoraes.tw@naturapay.net>
2021-09-12 16:44:57 -0300

    change wepack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,47 @@
-module.exports = (options, webpack) => {
-    const lazyImports = [
-        '@nestjs/microservices/microservices-module',
-        '@nestjs/websockets/socket-module',
-    ];
+const webpack = require('webpack');
+const WebpackConfigFactory = require('@nestjs/ng-universal')
+    .WebpackConfigFactory;
+const nodeExternals = require('webpack-node-externals');
 
-    return {
-        ...options,
-        externals: [],
-        output: {
-            libraryTarget: 'commonjs2',
+/**
+ * In fact, passing following configuration to the WebpackConfigFactory is not required
+ * default options object returned from this method has equivalent entries defined by default.
+ *
+ * Example: WebpackConfigFactory.create(webpack);
+ */
+const webpackOptions = WebpackConfigFactory.create(webpack, {
+    // This is our Nest server for Dynamic universal
+    server: './server/main.ts',
+});
+
+const whitelistedPackages = /^(?!(fastify-swagger)).*/;
+webpackOptions.externals[1] = nodeExternals({
+    allowlist: whitelistedPackages,
+});
+
+webpackOptions.output = {
+    libraryTarget: 'commonjs2',
+}
+
+webpackOptions.plugins.push(
+    new webpack.IgnorePlugin({
+        checkResource(resource) {
+            const lazyImports = [
+                '@nestjs/microservices/microservices-module',
+                '@nestjs/websockets/socket-module',
+            ];
+            console.log( resource )
+            if (!lazyImports.includes(resource)) {
+                return false;
+            }
+            try {
+                require.resolve(resource);
+            } catch (err) {
+                return true;
+            }
+            return false;
         },
-        plugins: [
-            ...options.plugins,
-            new webpack.IgnorePlugin({
-                checkResource(resource) {
-                    if (lazyImports.includes(resource)) {
-                        try {
-                            require.resolve(resource);
-                        } catch (err) {
-                            return true;
-                        }
-                    }
-                    return false;
-                },
-            }),
-        ],
-    };
-};
+    }),
+);
+
+module.exports = webpackOptions;
